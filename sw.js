cf9ba27ead2fbfe7479909fae309cb01a9e3120f
buxogff <2.80100104e+08+buxogff@users.noreply.github.com>
2026-09-24 19:24:26 +0000

    Fix direct routes and Firebase web auth

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kickup-football-v1";
+const CACHE_NAME = "kickup-football-v2";
 const APP_SHELL = ["./", "./manifest.json", "./kickup-icon-192.png", "./kickup-icon-512.png"];
 
 self.addEventListener("install", (event) => {
